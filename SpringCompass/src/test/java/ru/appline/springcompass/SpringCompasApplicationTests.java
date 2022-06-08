64e502525e1294cf37663ca0aebbc703325a099b
Nail Fakhrtdinov <fakh-nail@yandex.ru>
2022-06-08 08:50:19 +0300

    Compass

---
+++ SpringCompass/src/test/java/ru/appline/springcompass/SpringCompasApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.appline.springcompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
